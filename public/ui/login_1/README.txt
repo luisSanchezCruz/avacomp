ead3d4be57704df902abde8f302fa632d6710f0c
luis sanchez <luis.sanchez29@hotmail.es>
2021-02-01 05:08:37 -0400

    first commit

---
+++ public/ui/login_1/README.txt
@@ -0,0 +1,2 @@
+avacomp login 1
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="jumbotron bg-dark text-light">
+        <div className="container">
+          <h1 class="display-4">Avacomp ui library</h1>
+        </div>
+      </div>
+
+      <ul class="list-group">
+        <li className="list-group-item">
+          Login 1 (Responsive)
+          <ul className="list-group">
+            <li className="list-group-item">
+              <a href="./ui/login_1/login/login.html">Main</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_1/login_dark_mode/login.html">Dark mode </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_1/login_loading/login.html">Loading </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_1/login_round_social_media/login.html">
+                Round Social Media{" "}
+              </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_1/login_validation/login.html">Validation </a>
+            </li>
+          </ul>
+        </li>
+
+        <li className="list-group-item">
+          Login 2 (Responsive)
+          <ul className="list-group">
+            <li className="list-group-item">
+              <a href="./ui/login_2/login/login.html">Main</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_2/login_dark_mode/login.html">Dark mode </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_2/login_loading/login.html">Loading </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_2/login_validation/login.html">Validation</a>
+            </li>
+          </ul>
+        </li>
+
+        <li className="list-group-item">
+          Login 3 Pink Theme (Responsive)
+          <ul className="list-group">
+            <li className="list-group-item">
+              <a href="./ui/login_3/login/login.html">Main</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_3/login_loading/login.html">Loading </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_3/login_validation/login.html">Validation </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_3/login_android_tablet_380X640/login.html">
+                Android Tablet 380X640
+              </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_3/login_android_tablet_571X960/login.html">
+                Android Tablet 571X960
+              </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_3/login_android_tablet_760X1280/login.html">
+                Android Tablet 760X1280
+              </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_3/login_android_tablet_1140X1920/login.html">
+                Android Tablet 1140X1920
+              </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_3/login_android_tablet_1520X2560/login.html">
+                Android Tabet 1520X2560
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li className="list-group-item">
+          Login 4 Blue Theme (Responsive)
+          <ul className="list-group">
+            <li className="list-group-item">
+              <a href="./ui/login_4/login/login.html">Main</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_4/login_loading/login.html">Loading </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_4/login_validation/login.html">Validation </a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_4/login_ipad_834X1112/login.html">Ipad 834X1112</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_4/login_ipad_1668X2224/login.html">Ipad 1668X2224</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_4/login_ipad_2502X3336/login.html">Ipad 2502X3336</a>
+            </li>
+          </ul>
+        </li>
+
+
+        <li className="list-group-item">
+          Login Desktop (Responsive)
+          <ul className="list-group">
+            <li className="list-group-item">
+              <a href="./ui/login_desktop/login/login.html">Main Round social media</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_desktop/login_basic/login.html">Basic</a>
+            </li>
+         
+            <li className="list-group-item">
+              <a href="./ui/login_desktop/login_dark_mode/login.html">Dark Mode</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_desktop/login_dark_mode_left/login.html">Dark Mode Left</a>
+            </li>
+            <li className="list-group-item">
+              <a href="./ui/login_desktop/login_left/login.html">Login Left</a>
+            </li>
+         
+          </ul>
+        </li>
+
+
+      </ul>
     </div>
   );
 }
